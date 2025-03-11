164ab1b48fcc68b490c5c2b46dca332506076b17
William Santana <williamandrade1058@gmail.com>
2025-03-11 13:57:57 -0300

    primeiros passos

---
+++ js/basico/primeiros_passos/main.js
@@ -0,0 +1,16 @@
+alert("Teste");
+console.log("Hello World!");
+console.log("Essa mensagem é do JS conectado ao HTML");
+
+// sintaxe correta
+console.log();
+
+/* 
+    sintaxe incorreta
+    con.sol.e.log();
+
+    No Js o ponto e vírgula é opcional
+*/
+
+console.log("Hello World!");
+console.log("Essa mensagem é do JS conectado ao HTML");
