9cf4c343b05bce56dd8c82990e1132adc0240add
William Santana <williamandrade1058@gmail.com>
2025-06-01 23:16:01 -0300

    tópico sobre strict mode

--- js/javascript_antes_do_framework/js_moderno/conhecendo_strict_mode/scripts.js
+++ js/javascript_antes_do_framework/js_moderno/conhecendo_strict_mode/scripts.js
@@ -1,1 +1,34 @@
-alert("teste");
+/*
+    O strict mode (modo restrito): 
+    ativando esse modo, os erros que 
+    eram silenciosos passa a gerar exceções no JavaScript. 
+*/
+"use strict";
+
+function showMessage() {
+  let personName = "William";
+  console.log("Olá ", personName);
+}
+
+showMessage();
+
+class Student {
+  get point() {
+    return 7;
+  }
+}
+
+// Tenta mudar uma propriedade de leitura.
+let student = new Student();
+// student.point = 10;
+
+// Tentando deletar uma propriedade de um objeto que não posso deletar.
+// delete window.document;
+
+// Quando passamos parâmetros duplicados.
+function sum(a = Number, a = Number, c = Number) {
+  return a + a + c;
+}
+
+const result = sum(1, 3, 2); // 3 + 3 + 2 = 8
+console.log(result);
