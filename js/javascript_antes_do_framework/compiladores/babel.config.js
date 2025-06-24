43fa946a049e57b70a01ac1dc668d3da387c283a
William Santana <williamandrade1058@gmail.com>
2025-06-24 20:30:56 -0300

    Configurando targets

--- js/javascript_antes_do_framework/compiladores/babel.config.js
+++ js/javascript_antes_do_framework/compiladores/babel.config.js
@@ -1,3 +1,15 @@
-const presets = ["@babel/preset-env"];
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      targets: {
+        firefox: "17",
+        edge: "60",
+        chrome: "67",
+        safari: "11.1",
+      },
+    },
+  ],
+];
 
 module.exports = { presets };
