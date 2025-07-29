364879709c4eee828112458fb60b9d973b3f7581
William Santana <williamandrade1058@gmail.com>
2025-07-29 14:43:21 -0300

    Verificação de tipo estático

---
+++ ts/conhecendo_o_typescript/Verificacao_de_tipo_estatico/main.ts
@@ -0,0 +1,23 @@
+// const message = "Hello TypeScript";
+let message = "Hello TypeScript";
+
+// Consigo mudar?
+message = "TypeScript";
+
+function showMessage() {
+  console.log(message);
+}
+
+// Consigo executar?
+// message();
+
+showMessage();
+
+const user = {
+  name: "William Batista",
+  email: "wbs@gmail.com",
+};
+
+// Propriedade existe?
+// console.log(user.avatar)
+console.log(user.name);
