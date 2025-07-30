1ac148d600d5732e9944409b6595afaf72181485
William Santana <williamandrade1058@gmail.com>
2025-07-30 11:08:03 -0300

    tipos_primitivos

---
+++ ts/tipos_primitivos/tipos_pimitivos/main.ts
@@ -0,0 +1,16 @@
+// String para texto.
+let username: string;
+
+username = "William";
+// username = 17;
+// username = true;
+
+// Number para número.
+let total: number;
+total = 3;
+total = 7.5;
+
+// Boolean para verdadeiro ou falso
+let isLoading: boolean;
+isLoading = true;
+isLoading = false;
