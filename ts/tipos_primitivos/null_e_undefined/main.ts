22c9a9aaa05f60c1237aaa9c6ff9c7998090613c
William Santana <williamandrade1058@gmail.com>
2025-07-30 12:22:50 -0300

    null e undefined

---
+++ ts/tipos_primitivos/null_e_undefined/main.ts
@@ -0,0 +1,18 @@
+// undefined
+
+let value: number; // undefined
+value = 12;
+console.log(value);
+
+let user: { name: string } = {
+  name: "William",
+};
+// console.log(user.email);
+
+// null
+let email = null;
+console.log(email);
+
+if (!email) {
+  console.log("Informe o email!");
+}
