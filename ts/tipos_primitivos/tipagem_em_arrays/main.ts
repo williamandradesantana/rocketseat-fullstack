bcaa975350bca9c37a5147318603eef5ce931d48
William Santana <williamandrade1058@gmail.com>
2025-07-30 11:31:19 -0300

    tipagem em arrays

---
+++ ts/tipos_primitivos/tipagem_em_arrays/main.ts
@@ -0,0 +1,6 @@
+// Tipagem explícita ou Anotação de tipo (Type Annotation)
+let names: string[] = ["William", "Mayke"];
+let numbers: number[] = [1, 24, 312];
+
+// Inferência de tipo
+let products = ["Product x", "Product y"];
