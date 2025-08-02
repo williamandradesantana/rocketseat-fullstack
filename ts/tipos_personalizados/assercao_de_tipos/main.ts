15677306b904723a2299a31494e3005eec1349c6
William Santana <williamandrade1058@gmail.com>
2025-08-02 13:59:30 -0300

    Asserção de tipos

---
+++ ts/tipos_personalizados/assercao_de_tipos/main.ts
@@ -0,0 +1,12 @@
+// Comum para utilizar em consumo de API's
+
+type UserResponse = {
+  id: number;
+  name: string;
+  avatar: string;
+};
+
+let userResponse = {} as UserResponse;
+userResponse.avatar;
+userResponse.id;
+userResponse.name;
