c8832a9c0a3247d12da560e5f73aa8d25505a97f
William Santana <williamandrade1058@gmail.com>
2025-08-12 14:09:43 -0300

    Utilizando Status Code

--- node/fundamentos_api/api/src/server.js
+++ node/fundamentos_api/api/src/server.js
@@ -2,7 +2,8 @@ import http from "node:http";
 
 const server = http.createServer((request, response) => {
   const { method } = request;
-  return response.end("Método: " + method);
+
+  return response.writeHead(200).end("Método: " + method);
 });
 
 server.listen(3333);
