e1d07e51982f01d2f5c7a7f16c8ad87d2f91bae8
William Santana <williamandrade1058@gmail.com>
2025-08-12 14:04:12 -0300

    Identificando o método HTTP

--- node/fundamentos_api/api/src/server.js
+++ node/fundamentos_api/api/src/server.js
@@ -1,7 +1,8 @@
 import http from "node:http";
 
 const server = http.createServer((request, response) => {
-  return response.end("Minha primeira api");
+  const { method } = request;
+  return response.end("Método: " + method);
 });
 
 server.listen(3333);
