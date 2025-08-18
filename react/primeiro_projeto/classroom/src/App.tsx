b35173c8db1fa808aa4b5d8447ad25465423bfbf
William Santana <williamandrade1058@gmail.com>
2025-08-18 14:42:57 -0300

    criando componente de letter

--- react/primeiro_projeto/classroom/src/App.tsx
+++ react/primeiro_projeto/classroom/src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
 
 import styles from "./app.module.css";
 
@@ -14,6 +15,12 @@ export default function App() {
         <Header current={5} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Linguagem de programação dinâmica" />
+
+        <Letter value="R" />
+        <Letter value="E" />
+        <Letter value="A" />
+        <Letter value="C" />
+        <Letter value="T" />
       </main>
     </div>
   );
