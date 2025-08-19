7b26abc4437c1e41968365e99029fd9c37f0b375
William Santana <williamandrade1058@gmail.com>
2025-08-19 12:13:58 -0300

    Criando componente de INput

--- react/primeiro_projeto/classroom/src/App.tsx
+++ react/primeiro_projeto/classroom/src/App.tsx
@@ -3,6 +3,7 @@ import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 
 import styles from "./app.module.css";
+import { Input } from "./components/Input";
 
 export default function App() {
   function handleRestartGame() {
@@ -22,6 +23,12 @@ export default function App() {
           <Letter value="A" />
           <Letter value="C" />
           <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
         </div>
       </main>
     </div>
