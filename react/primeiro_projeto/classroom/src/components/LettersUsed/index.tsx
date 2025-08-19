8694669b0604bf5a543cb0aad0f6a120265c1011
William Santana <williamandrade1058@gmail.com>
2025-08-19 12:42:14 -0300

    Variações de Tamanho de Letra

--- react/primeiro_projeto/classroom/src/components/LettersUsed/index.tsx
+++ react/primeiro_projeto/classroom/src/components/LettersUsed/index.tsx
@@ -1,9 +1,16 @@
 import styles from "./styles.module.css";
 
+import { Letter } from "../Letter";
+
 export function LettersUsed() {
   return (
     <div className={styles.lettersUsed}>
       <h5>Letras Utilizadas</h5>
+
+      <div>
+        <Letter value="R" size="small" />
+        <Letter value="X" size="small" />
+      </div>
     </div>
   );
 }
