da91836c6eb648b6cf3b4448e14473d2b05fb848
William Santana <williamandrade1058@gmail.com>
2025-08-25 10:19:59 -0300

    Rota de Produtos

--- react/avancando_em_react/navegacao/classroom/src/routes/AppRoutes.tsx
+++ react/avancando_em_react/navegacao/classroom/src/routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
 
 import { Home } from "../pages/Home";
+import { Products } from "../pages/Products";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
