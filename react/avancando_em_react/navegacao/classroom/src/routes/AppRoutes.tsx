5e6921e9e2fd190e33c498f1a908d04c020b0a6e
William Santana <williamandrade1058@gmail.com>
2025-08-25 10:36:51 -0300

    Página 404

--- react/avancando_em_react/navegacao/classroom/src/routes/AppRoutes.tsx
+++ react/avancando_em_react/navegacao/classroom/src/routes/AppRoutes.tsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router";
 
 import { Home } from "../pages/Home";
 import { Products } from "../pages/Products";
+import { NotFound } from "../pages/NotFound";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
       <Route path="/products" element={<Products />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
