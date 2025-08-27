2aab81f6da77810afec48864d9dc643c1883a4a2
William Santana <williamandrade1058@gmail.com>
2025-08-27 16:55:23 -0300

    useParams

--- react/avancando_em_react/navegacao/classroom/src/pages/Details.tsx
+++ react/avancando_em_react/navegacao/classroom/src/pages/Details.tsx
@@ -1,7 +1,17 @@
+import { useParams } from "react-router";
+
 export function Details() {
+  const { id } = useParams();
+
   return (
     <div>
       <h1>Detalhes</h1>
+      <span>
+        ID do produto: <strong>{id}</strong>
+      </span>
+
+      <a href="/">Home</a>
+      <a href="/products">Produtos</a>
     </div>
   );
 }
--- react/avancando_em_react/navegacao/classroom/src/pages/Products.tsx
+++ react/avancando_em_react/navegacao/classroom/src/pages/Products.tsx
@@ -1,8 +1,11 @@
 import { useSearchParams } from "react-router";
+import { useNavigate } from "react-router";
 
 export function Products() {
   const [searchParams] = useSearchParams();
   const category = searchParams.get("category");
+
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -15,6 +18,12 @@ export function Products() {
           Categoria <strong>{category}</strong>
         </span>
       )}
+
+      <div className="cards">
+        <a onClick={() => navigate("/details/1")}>Produto 1</a>
+        <a onClick={() => navigate("/details/2")}>Produto 2</a>
+        <a onClick={() => navigate("/details/3")}>Produto 3</a>
+      </div>
     </div>
   );
 }
