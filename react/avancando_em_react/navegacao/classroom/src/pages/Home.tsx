65bbb9fb32e3e6042c93aad694a95c5b235643df
William Santana <williamandrade1058@gmail.com>
2025-08-27 16:32:51 -0300

    useNavigate

--- react/avancando_em_react/navegacao/classroom/src/pages/Home.tsx
+++ react/avancando_em_react/navegacao/classroom/src/pages/Home.tsx
@@ -1,4 +1,12 @@
+import { useNavigate } from "react-router";
+
 export function Home() {
+  const navigate = useNavigate();
+
+  function productsOpen() {
+    navigate("/products");
+  }
+
   return (
     <div>
       <h1>Página Home</h1>
@@ -6,6 +14,10 @@ export function Home() {
       <nav>
         <a href="/products">Produtos</a>
         <a href="/products?category=tvs">Categoria</a>
+
+        <button onClick={productsOpen} type="button">
+          Ver produtos
+        </button>
       </nav>
     </div>
   );
