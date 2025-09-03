58fcd84354461a487c296176ce372b09ce050ad4
William Santana <williamandrade1058@gmail.com>
2025-09-03 18:56:44 -0300

    Retornando para Página Anterior

--- react/avancando_em_react/navegacao/classroom/src/pages/Details.tsx
+++ react/avancando_em_react/navegacao/classroom/src/pages/Details.tsx
@@ -1,17 +1,20 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 export function Details() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   return (
     <div>
+      <button type="button" onClick={() => navigate(-1)}>
+        Voltar
+      </button>
+
       <h1>Detalhes</h1>
+
       <span>
         ID do produto: <strong>{id}</strong>
       </span>
-
-      <a href="/">Home</a>
-      <a href="/products">Produtos</a>
     </div>
   );
 }
